ecc4d300c0310318cc0e83e2c6caab45127f1e6c
semanticker <semanticker@gmail.com>
2020-02-25 07:55:47 +0900

    CustomLoginSuccessHandler 처리

--- src/main/java/org/zerock/controller/CommonController.java
+++ src/main/java/org/zerock/controller/CommonController.java
@@ -17,5 +17,20 @@ public class CommonController {
 		
 		model.addAttribute("msg", "Assess Denied");
 	}
+	
+	@GetMapping("/customLogin")
+	public void loginInput(String error, String logout, Model model) {
+		log.info("error:" + error);
+		log.info("logout:" + logout);
+		
+		if(error != null) {
+			model.addAttribute("error", "Login Error Check Your Account");
+		}
+		
+		if(logout != null) {
+			model.addAttribute("logout", "Logout!!");
+		}
+	}
+	
 
 }
