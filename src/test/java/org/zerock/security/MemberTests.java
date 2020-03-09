24c2282021359163122427379d8bd8824dd6f489
semanticker <semanticker@gmail.com>
2020-03-10 07:22:12 +0900

    커스텀 UserDetails 사용

--- src/test/java/org/zerock/security/MemberTests.java
+++ src/test/java/org/zerock/security/MemberTests.java
@@ -1,123 +1,33 @@
 package org.zerock.security;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-
-import javax.sql.DataSource;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.MemberVO;
+import org.zerock.mapper.MemberMapper;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({
-	"file:/Users/semanticker/aluna/workspace_sts2/ex06/src/main/webapp/WEB-INF/spring/root-context.xml",
-	"file:/Users/semanticker/aluna/workspace_sts2/ex06/src/main/webapp/WEB-INF/spring/security-context.xml"
+	"file:/Users/semanticker/aluna/workspace_sts2/ex06/src/main/webapp/WEB-INF/spring/root-context.xml"
 })
 @Log4j
 public class MemberTests {
 
 	@Setter(onMethod_ = @Autowired)
-	private PasswordEncoder pwencoder;
-	
-	
-	@Setter(onMethod_ = @Autowired)
-	private DataSource ds;
-	
-	//@Test
-	public void testInsertMemeber() {
-		String sql = "insert into zr_tbl_member(userid, userpw, username) values (?,?,?)";
-		
-		for(int i=0; i<100; i++) {
-			Connection conn = null;
-			PreparedStatement pstmt = null;
-			
-			try {
-				
-				conn = ds.getConnection();
-				pstmt = conn.prepareStatement(sql);
-				
-				pstmt.setString(2, pwencoder.encode("pw"+i));
-				
-				if(i<80) {
-					pstmt.setString(1, "user"+i);
-					pstmt.setString(3, "일반사용자"+i);
-				}else if(i<90) {
-					pstmt.setString(1, "manager"+i);
-					pstmt.setString(3, "운영자"+i);
-				}else {
-					pstmt.setString(1, "admin"+i);
-					pstmt.setString(3, "관리자"+i);
-				}
-				
-				pstmt.executeUpdate();
-				
-			}catch(Exception e) {
-				e.printStackTrace();
-			}finally {
-				if(pstmt != null) {
-					try {
-						pstmt.close();
-					}catch(Exception e) {}
-				}
-				
-				if(conn != null) {
-					try {
-						conn.close();
-					} catch(Exception e) {}
-				}
-			}
-		}
-	}
+	private MemberMapper mapper;
 	
 	@Test
-	public void testInsertAuth() {
-		String sql = "insert into zr_tbl_member_auth(userid, auth) values(?,?)";
+	public void testRead() {
+		MemberVO vo = mapper.read("admin90");
 		
-		for(int i=0; i<100; i++) {
-			Connection conn = null;
-			PreparedStatement pstmt = null;
-			
-			try {
-				
-				conn = ds.getConnection();
-				pstmt = conn.prepareStatement(sql);
-				
-				if(i<80) {
-					pstmt.setString(1, "user"+i);
-					pstmt.setString(2, "ROLE_USER");
-				}else if(i<90) {
-					pstmt.setString(1, "manager"+i);
-					pstmt.setString(2, "ROLE_MANAGER");
-				}else {
-					pstmt.setString(1, "admin"+i);
-					pstmt.setString(2, "ROLE_ADMIN");
-				}
-				
-				pstmt.executeUpdate();
-				
-			}catch(Exception e) {
-				e.printStackTrace();
-			}finally {
-				if(pstmt != null) {
-					try {
-						pstmt.close();
-					}catch(Exception e) {}
-				}
-				
-				if(conn != null) {
-					try {
-						conn.close();
-					} catch(Exception e) {}
-				}
-			}
-		}
+		log.info(vo);
+		
+		vo.getAuthList().forEach(authVO -> log.info(authVO));
 	}
 }
 
